c2a27aa6b753b028840390a8d026027194c0728b
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-30 10:50:35 -0700

    Updated NuGet Packages

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace DynamicStackLayoutSize.iOS
 {
-    [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-    {
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            global::Xamarin.Forms.Forms.Init();
+	[Register(nameof(AppDelegate))]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+		{
+			global::Xamarin.Forms.Forms.Init();
 
-            LoadApplication(new App());
+			LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
-        }
-    }
+			return base.FinishedLaunching(uiApplication, launchOptions);
+		}
+	}
 }
